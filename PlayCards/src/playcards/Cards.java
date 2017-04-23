be91d3e92343bf356d5d9fbb6884d6750a5926ca
GertKruger <GertKruger@gerts-mbp>
2017-04-23 20:18:37 +0200

    Phase 1 complete

--- PlayCards/src/playcards/Cards.java
+++ PlayCards/src/playcards/Cards.java
@@ -15,40 +15,121 @@ import java.util.Random;
 public class Cards {
     public final String[] cardSuit = {"H", "S", "D", "C"};  //Hearts, Spades, Diamonds, Clubs
     public final String[] cardValue = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-    ArrayList<String> deck = new ArrayList<>();  //Cards will be stored as a string in format <value><suit> eg "2D", "QH" 
-
-    // return a new, unshuffled deck
-public ArrayList<String> newDeck() {
-    ArrayList<String> printDeck = new ArrayList<>(); 
+    public ArrayList<String> deck = new ArrayList<>();  //Cards will be stored as a string in format <value><suit> eg "2D", "QH" 
+    
+    // make a new, unshuffled deck
+public void newDeck() {
     
     for(String suit: cardSuit) {
         for(String value: cardValue) {
-            printDeck.add( value.concat(suit) );
+            deck.add( value.concat(suit) );
         }
     }
-        return printDeck;
     }
 
     // shuffle deck of cards passed as @param and return
-public ArrayList<String> shuffleDeck(ArrayList<String> originalDeck) {
+public void shuffleDeck() {
     // implementation of perfectly randomised shuffle
     // as per Persi Diaconis (Stanford University) on card shuffling [ https://youtu.be/AxJubaijQbI]
     // Summary of method: [Note bottom card. Take top card and place randomly in deck. 
     // Repreat until original bottom card is now on top. Shuffle this card in randomly and end]
-    int deckSize = originalDeck.size();
-    String lastCard =  originalDeck.get( deckSize ); // must remember last card for shuffle completion
-    ArrayList<String> shuffledDeck = (ArrayList<String>) originalDeck.clone();
+    int deckSize = deck.size();
+    String lastCard =  deck.get( deckSize - 1); // must remember last card for shuffle completion
+    String currentCard;  //card being shuffled
+    boolean lastOnTop = false;
+    boolean finalPass = false;
+    ArrayList<String> shuffledDeck = (ArrayList<String>) deck.clone();
     Random rand = new Random();
+    int placePosition;  //position to insert currentCard
+    int shufflesCount = 0;  
     
+    while (!finalPass) {
+        // draw top card
+        currentCard = shuffledDeck.get(0);
+        shuffledDeck.remove(0);
+        
+        if(lastOnTop) { finalPass = true; }
+        // shuffle card in to deck
+        placePosition = rand.nextInt(deckSize - 1) + 1;
+        if (placePosition < deckSize - 1) {
+            shuffledDeck.add(placePosition, currentCard);
+        } else { shuffledDeck.add(currentCard); }
+        
+        // check for last card on top
+        if( shuffledDeck.indexOf(lastCard) == 0) { lastOnTop = true; }
+        shufflesCount ++;    
+    }
     
-    
-    
-    
+    deck = (ArrayList<String>) shuffledDeck.clone();
+    System.out.println("Shuffles to randomise the deck : " + shufflesCount );
 }
 
+    // draw a card from top of deck and remove card from deck
+public String drawTopCard() {
+    String drawnCard = deck.get(0);
+    deck.remove(0);
+    
+    return(drawnCard);
+}
+
+// draw a random card from within the deck
+public String drawRandomCard() {
+    Random rand = new Random(); 
+    int drawPosition = rand.nextInt(deck.size() - 1);
+
+    String drawnCard = deck.get(drawPosition);
+    deck.remove(drawPosition);
+    
+    return(drawnCard);          
+            }
 
 
+// add card to deck
+public void addCardToDeck( String card) {
+    deck.add(card);
+}
 
+// transfer n cards from source to target
+public void transferCardsTo(Cards target, int n ) {
+    
+    for(int i=1; i <= n; i++ ) {
+        target.deck.add( this.deck.get(0) );
+        this.deck.remove(0);
+    }
+}
+
+// transfer specific cards from source to target based on position
+public void transferThisCardTo(Cards target, int cardPos ) {
+        if ( deck.size() > cardPos ) {
+        target.deck.add( this.deck.get(cardPos) );
+        this.deck.remove(cardPos);
+        } else { System.out.println("The requested card is out of range of this deck :" + cardPos); }
+
+}
+
+// transfer specific cards from source to target based on value
+public void transferThisCardTo(Cards target, String cardVal ) {
+        if( deck.contains(cardVal) ) {
+        target.deck.add( cardVal );
+        this.deck.remove(cardVal);
+        } else { System.out.println("This deck does not contain card :" + cardVal); }
+}
+
+    // print the card in deck to console in 
+public void printDeck() {
+        int cols = 10;
+        int colWidth = 5;
+        String sPad = new String(new char[colWidth]).replace("\0", " ");         
+        //repeated = new String(new char[n]).replace("\0", s);
+        int i = 0;
+        
+        for( String card: deck) {
+            i++;
+            System.out.print( (card + sPad).substring(0, colWidth));
+            if( i == cols ) { System.out.println(""); i = 0; }
+        }
+        System.out.println("");
+}    
 
 
 
--- PlayCards/src/playcards/PlayCards.java
+++ PlayCards/src/playcards/PlayCards.java
@@ -5,6 +5,9 @@
  */
 package playcards;
 
+
+import java.util.ArrayList;
+
 /**
  *
  * @author kruge
@@ -15,7 +18,35 @@ public class PlayCards {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Cards testDeck = new Cards();
+        Cards testHand1 = new Cards();
+        Cards testHand2 = new Cards();
+        Cards discardPile = new Cards();
+        
+        testDeck.newDeck();
+        testDeck.shuffleDeck();
+        testDeck.printDeck();
+        
+        testDeck.transferCardsTo(testHand1, 7);
+        testDeck.transferCardsTo(testHand2, 7);
+        System.out.print("Hand1 :"); testHand1.printDeck();
+        System.out.print("Hand2 :"); testHand2.printDeck();
+        System.out.println("Remaining deck :");;
+        testDeck.printDeck();
+        
+        testHand1.transferCardsTo(discardPile, 3);
+        testDeck.transferCardsTo(testHand1, 3);
+        testHand2.transferThisCardTo(discardPile, 1);
+        testDeck.transferCardsTo(testHand2, 1);        
+        testHand2.transferThisCardTo(discardPile, 3);
+        testDeck.transferCardsTo(testHand2, 1);      
+        testHand2.transferThisCardTo(discardPile, 5);
+        testDeck.transferCardsTo(testHand2, 1);      
+                
+        System.out.print("Hand1 :"); testHand1.printDeck();
+        System.out.print("Hand2 :"); testHand2.printDeck();
+        System.out.println("Remaining deck :");;
+        testDeck.printDeck();
     }
     
 }
